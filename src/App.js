d04e3663d3c28c3db349bf93f520cd70238327ea
VISHAL SAHOO <bhagis052@gmail.com>
2024-05-14 21:20:02 +0530

    Maastering react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+// https://jsonplaceholder.typicode.com/posts
+import { useState } from "react";
 
 function App() {
+  const [data, setData] = useState([]);
+  
+  const fetchData = async() => {
+    const res = await fetch("https://jsonplaceholder.typicode.com/posts");
+    const data = await res.json();
+    setData(data);
+  };
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h3>API Request & Passing Data</h3>
+      <button onClick ={fetchData}>Fetch All Data</button>
+      {data.map((data , index) =>{
+        return <Post key={index} title={data.title} body={data.body} />;
+      })}
+      </div>  
   );
 }
 
 export default App;
+
+function Post({title , body}) {
+  return(
+    <div class="card">
+      <div class="img-container">
+      <img
+          src="https://images.unsplash.com/photo-1610932793632-6ba00bdffee8?crop=entropy&cs=srgb&fm=jpg&ixid=M3wzMjM4NDZ8MHwxfHJhbmRvbXx8fHx8fHx8fDE3MTA2NzkwMDh8&ixlib=rb-4.0.3&q=85"
+          width="336"
+          alt=""
+        />
+      </div>
+      <h1>{title}</h1>
+      <p>{body}</p>
+    </div>
+  );
+}
